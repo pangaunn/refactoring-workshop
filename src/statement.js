36655e28f28635e1dcedc5fe728517e891dd9eb4
Naris Jareonsee <naris.j@linecorp.com>
2019-10-31 13:54:31 +0700

    setup prettier

--- src/statement.js
+++ src/statement.js
@@ -1,11 +1,11 @@
 function statement(invoice, plays) {
-let totalAmount = 0
-  let volumeCredits = 0
+  let totalAmount = 0
+  let volumeCredits = '0'
   let result = `Statement for ${invoice.customer}\n`
   const format = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-    minimumFractionDigits: 2,
+    minimumFractionDigits: 2
   }).format
   for (let perf of invoice.performances) {
     const play = plays[perf.playID]
