02ca5adbb968a4cbe955dad149822876daf1c30a
Pangaunn <aunn_it08@hotmail.com>
2019-10-31 16:30:33 +0700

    add step-24

---
+++ step-24/statement.js
@@ -0,0 +1,30 @@
+const createStatementData = require('./createStatementData.js')
+
+function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays))
+}
+
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`
+
+  for (let perf of data.performances) {
+    // print line for this order
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    } seats)\n`
+  }
+
+  result += `Amount owed is ${usd(data.totalAmount)}\n`
+  result += `You earned ${data.totalVolumeCredits} credits\n`
+  return result
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2
+    }).format(aNumber / 100)
+  }
+}
+
+module.exports = statement
