52f93bdfa8e7f4423852503744dd950451c11507
Pangaunn <aunn_it08@hotmail.com>
2019-10-31 16:02:55 +0700

    fix step-15

--- step-15/statement.js
+++ step-15/statement.js
@@ -13,19 +13,19 @@ function statement(invoice, plays) {
   return result
 
   function totalAmount() {
-    let totalAmount = 0
+    let result = 0
     for (let perf of invoice.performances) {
-      totalAmount += amountFor(perf)
+      result += amountFor(perf)
     }
-    return totalAmount
+    return result
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0
+    let result = 0
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf)
+      result += volumeCreditsFor(perf)
     }
-    return volumeCredits
+    return result
   }
 
   function usd(aNumber) {
