bd0f3f75d64178a7fa7e3194c7b19e3946b75fe1
Pangaunn <aunn_it08@hotmail.com>
2019-10-31 16:09:27 +0700

    add step-17

---
+++ step-17/statement.js
@@ -0,0 +1,81 @@
+function statement(invoice, plays) {
+  const statementData = {}
+  statementData.customer = invoice.customer
+  statementData.performances = invoice.performances
+  return renderPlainText(statementData, plays)
+}
+
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`
+
+  for (let perf of data.performances) {
+    // print line for this order
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    } seats)\n`
+  }
+
+  result += `Amount owed is ${usd(totalAmount())}\n`
+  result += `You earned ${totalVolumeCredits()} credits\n`
+  return result
+
+  function totalAmount() {
+    let result = 0
+    for (let perf of data.performances) {
+      result += amountFor(perf)
+    }
+    return result
+  }
+
+  function totalVolumeCredits() {
+    let result = 0
+    for (let perf of data.performances) {
+      result += volumeCreditsFor(perf)
+    }
+    return result
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2
+    }).format(aNumber / 100)
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0
+    result += Math.max(aPerformance.audience - 30, 0)
+    if ('comedy' === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5)
+    return result
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0
+    switch (playFor(aPerformance).type) {
+      case 'tragedy':
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience
+        break
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`)
+    }
+    return result
+  }
+}
+
+module.exports = statement
